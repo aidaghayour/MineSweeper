d69dfdfb265c5f4466a0099cd8b186091d3b9219
aidaghayour <aghayourkhia@mun.ca>
2024-05-04 12:35:21 -0400

    frames defined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import settings
+import utils
 
 root = Tk()
 
@@ -15,11 +16,15 @@ root.resizable(False,False)
 
 
 #add an frame element
-top_frame = Frame(root, bg='pink', width= 1440, height=180)
+top_frame = Frame(root, bg='pink', width= 1440, height=utils.height_prct(25))
 #where to place it
 top_frame.place(x=0,y=0)
-left_frame = Frame (root, bg="blue", width=360, height=540)
+
+left_frame = Frame (root, bg="blue", width=utils.width_prct(25), height=utils.height_prct(75))
 left_frame.place(x=0,y=180)
+
+center_frame = Frame(root, bg="GREEN", width=utils.width_prct(75),height=utils.height_prct(75) )
+center_frame.place(x=utils.width_prct(25), y=utils.height_prct(75))
 
 #Run the window
 root.mainloop()
