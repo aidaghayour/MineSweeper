8152fa6d577e6ba46eb9e16e8f65432a9bd8a3f1
aidaghayour <aghayourkhia@mun.ca>
2024-05-06 19:20:20 -0400

    Button object and events are created

--- cell.py
+++ cell.py
@@ -7,4 +7,15 @@ class Cell:
 
     def create_btn_object(self,location):
         btn = Button(location, text="text")
+
+        btn.bind('<Button-1>',self.left_click_actions) ## by convention <Button-1> means left click in Tinker!
+        btn.bind('<Button-3>',self.right_click_actions) ## by convention <Button-3> means right click in Tinker!
         self.cell_btn_object = btn
+
+    def left_click_actions(self,event):
+        print(event)
+        print("Left Clicked!")
+
+    def right_click_actions(self,event):
+        print(event)
+        print("right Clicked!")
