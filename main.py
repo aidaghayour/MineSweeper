34e43d442378c5ca9e812e0111a6f96c5d7b7f7d
aidaghayour <aghayourkhia@mun.ca>
2024-05-06 19:19:58 -0400

    cell class imported and a grid of buttons initated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import settings
 import utils
-
+from cell import Cell
 root = Tk()
 
 #set color
@@ -16,15 +16,22 @@ root.resizable(False,False)
 
 
 #add an frame element
-top_frame = Frame(root, bg='pink', width= 1440, height=utils.height_prct(25))
+top_frame = Frame(root, bg='pink',  width=settings.WIDTH,
+    height=utils.height_prct(25))
 #where to place it
 top_frame.place(x=0,y=0)
 
 left_frame = Frame (root, bg="blue", width=utils.width_prct(25), height=utils.height_prct(75))
-left_frame.place(x=0,y=180)
+left_frame.place(x=0, y=utils.height_prct(25))
 
 center_frame = Frame(root, bg="GREEN", width=utils.width_prct(75),height=utils.height_prct(75) )
-center_frame.place(x=utils.width_prct(25), y=utils.height_prct(75))
+center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
 
+
+for X in range(settings.GRID_SIZE):
+    for Y in range(settings.GRID_SIZE):
+        c = Cell()
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(column=X, row=Y)
 #Run the window
 root.mainloop()
