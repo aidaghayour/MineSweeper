f2031167fda5ea5d4225cab3a4e9bc9c4124a166
aidaghayour <aghayourkhia@mun.ca>
2024-05-07 17:59:26 -0400

    The surrounding cell calculations added

--- cell.py
+++ cell.py
@@ -22,6 +22,46 @@ class Cell:
     def left_click_actions(self,event):
         if self.is_mine:
             self.show_mine()
+        else:
+            if self.surrounded_cells_mines_length == 0:
+                for cell_obj in self.surrounded_cells:
+                    cell_obj.show_cell()
+            self.show_cell()
+    
+
+    def get_cell_by_axis(self,x,y):
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+
+    @property
+    def surrounded_cells(self):
+        cells = [
+            self.get_cell_by_axis(self.x - 1, self.y -1),
+            self.get_cell_by_axis(self.x - 1, self.y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y + 1)
+        ]
+        # List comprehension
+        cells = [cell for cell in cells if cell is not None]
+        return cells
+    
+    @property
+    def surrounded_cells_mines_length(self):
+        counter = 0
+        for cell in self.surrounded_cells:
+            if cell.is_mine:
+                counter += 1 
+
+        return counter
+
+    def show_cell(self):
+        print(self.cell_btn_object.configure(text=self.surrounded_cells_mines_length))
+
 
     def show_mine(self):
         # A logic to interrupt the game and display lost message
